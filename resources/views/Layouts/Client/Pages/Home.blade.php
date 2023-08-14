54cf2d4f7b6a011043e675466e698610490677f1
phult15 <phult15@fpt.edu.com>
2023-08-14 09:06:49 +0700

    asd

--- resources/views/Layouts/Client/Pages/Home.blade.php
+++ resources/views/Layouts/Client/Pages/Home.blade.php
@@ -77,52 +77,81 @@
                 </div>
             </div>
             <div class="HomeCategory">
-            <div class="HomeCategoryTitle">
-                <h3>There's something for everyone!</h3>
+                <div class="HomeCategoryTitle">
+                    <h3>There's something for everyone!</h3>
+                </div>
+                <div class="HomeCategoryContent">
+                    <div class="HomeBoxContent">
+                        <div class="imgContent">
+                            <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
+                        </div>
+                        <div class="infoContent">
+                            <h4>Hiso Party</h4>
+                        </div>
+                    </div>
+                    <div class="HomeBoxContent">
+                        <div class="imgContent">
+                            <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
+                        </div>
+                        <div class="infoContent">
+                            <h4>Hiso Party</h4>
+                        </div>
+                    </div>
+                    <div class="HomeBoxContent">
+                        <div class="imgContent">
+                            <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
+                        </div>
+                        <div class="infoContent">
+                            <h4>Hiso Party</h4>
+                        </div>
+                    </div>
+                    <div class="HomeBoxContent">
+                        <div class="imgContent">
+                            <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
+                        </div>
+                        <div class="infoContent">
+                            <h4>Hiso Party</h4>
+                        </div>
+                    </div>
+                    <div class="HomeBoxContent">
+                        <div class="imgContent">
+                            <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
+                        </div>
+                        <div class="infoContent">
+                            <h4>Hiso Party</h4>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="HomeCategoryContent">
-                <div class="HomeBoxContent">
-                    <div class="imgContent">
-                        <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
-                    </div>
-                    <div class="infoContent">
-                        <h4>Hiso Party</h4>
-                    </div>
+            <div class="HomeWhyChoise">
+                <div class="Hometitle">
+                    <h3>Vì sao bạn nên Order trên GrabFood?</h3>
                 </div>
-                <div class="HomeBoxContent">
-                    <div class="imgContent">
-                        <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
-                    </div>
-                    <div class="infoContent">
-                        <h4>Hiso Party</h4>
-                    </div>
-                </div>
-                <div class="HomeBoxContent">
-                    <div class="imgContent">
-                        <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
-                    </div>
-                    <div class="infoContent">
-                        <h4>Hiso Party</h4>
-                    </div>
-                </div>
-                <div class="HomeBoxContent">
-                    <div class="imgContent">
-                        <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
-                    </div>
-                    <div class="infoContent">
-                        <h4>Hiso Party</h4>
-                    </div>
-                </div>
-                <div class="HomeBoxContent">
-                    <div class="imgContent">
-                        <img src="https://d1sag4ddilekf6.cloudfront.net/compressed_webp/merchants/5-C26XTADAJBJFWA/hero/7f0ae9e274f842c1828d160643d2afda_1670796869756664822.webp" alt="">
-                    </div>
-                    <div class="infoContent">
-                        <h4>Hiso Party</h4>
-                    </div>
+                <div class="HomeContentWhyChoise">
+
+                    <ul>
+                        <li>
+                            <span><b>Nhanh nhất</b>
+                            <span> - GrabFood cung cấp  dịch vụ giao đồ ăn nhanh nhất thị trường.</span>
+                        </li>
+                        <li>
+                            <span><b>Nhanh nhất</b>
+                            <span> - Giờ đây, bạn chỉ cần thực hiện vài cú nhấp chuột hoặc chạm nhẹ là đã có thể đặt đồ ăn. Hãy đặt đồ ăn trực tuyến hoặc tải xuống siêu ứng dụng Grab của chúng tôi để có trải nghiệm nhanh hơn và thú vị hơn.</span>
+                        </li>
+                        <li>
+                            <span><b>Nhanh nhất</b>
+                            <span> - GrabFood cung cấp dịch vụ giao đồ ăn nhanh nhất thị trường.</span>
+                        </li>
+                    </ul>
+
+                    {{-- <p><i class="fa-solid fa-check" style="color: #1e781c;"></i> </span></p>
+                    <p><i class="fa-solid fa-check" style="color: #1e781c;"></i><span><b>Dễ dàng nhất</b> - Giờ đây, bạn chỉ cần thực hiện vài cú nhấp chuột hoặc chạm nhẹ là đã có thể đặt đồ ăn. Hãy đặt đồ ăn trực tuyến hoặc tải xuống siêu ứng dụng Grab của chúng tôi để có trải nghiệm nhanh hơn và thú vị hơn.</span></p>
+                    <p><i class="fa-solid fa-check" style="color: #1e781c;"></i><span><b>Đáp ứng mọi nhu cầu</b> - GrabFood cung cấp dịch vụ giao đồ ăn nhanh nhất thị trường.</span></p>
+                    <p><i class="fa-solid fa-check" style="color: #1e781c;"></i><span><b>Thanh toán dễ dàng</b> - GrabFood cung cấp dịch vụ giao đồ ăn nhanh nhất thị trường.</span></p>
+                    <p><i class="fa-solid fa-check" style="color: #1e781c;"></i><span><b>Nhiều quà tặng hơn</b> - GrabFood cung cấp dịch vụ giao đồ ăn nhanh nhất thị trường.</span></p> --}}
                 </div>
             </div>
-        </div>
+
         </div>
     
         
